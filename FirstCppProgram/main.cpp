219fae275282a72d9fb82e5acb0e8c56cbea3b89
LeoPazEs <estevam.leonardopaz@gmail.com>
2023-10-24 19:01:48 -0300

    for and std::cout
    Signed-off-by: LeoPazEs <estevam.leonardopaz@gmail.com>

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -7,5 +7,12 @@ consteval int get_value(){
 int main(){
     constexpr int value = get_value();
     std::cout << "value : " << value << std::endl;
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout << "My number is : " << i << std::endl;
+    }
+    
+
     return 0;
 }
