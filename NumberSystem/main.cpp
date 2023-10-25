8673654240cc0a53f4ca4793cb51b67d8df6f2e4
Leonardo Paz <estevam.leonardopaz@gmail.com>
2023-10-25 10:00:42 -0300

    numbers
    Signed-off-by: Leonardo Paz <estevam.leonardopaz@gmail.com>

---
+++ NumberSystem/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+    int numberDecimal = 15;
+    int numberOctal = 017;
+    int numberHex = 0x0f;
+    int numberBin = 0b00001111;
+
+    std::cout << "Number Decimal: " << numberDecimal << std::endl;
+    std::cout << "Number Octal: " << numberOctal << std::endl;
+    std::cout << "Number Hex: " << numberHex << std::endl;
+    std::cout << "Number Binary: " << numberBin << std::endl;
+    
+
+    return 0;
+}
