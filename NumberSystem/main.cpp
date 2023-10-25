44b0caa424b1dcf9804ca98f3834de2c81ecb27e
Leonardo Paz <estevam.leonardopaz@gmail.com>
2023-10-25 10:19:58 -0300

    pattern
    Signed-off-by: Leonardo Paz <estevam.leonardopaz@gmail.com>

--- NumberSystem/main.cpp
+++ NumberSystem/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-int main(){
-    int numberDecimal = 15;
-    int numberOctal = 017;
-    int numberHex = 0x0f;
-    int numberBin = 0b00001111;
+int main()
+{
+    int number_decimal = 15;
+    int number_octal = 017;
+    int number_hex = 0x0f;
+    int number_bin = 0b00001111;
 
-    std::cout << "Number Decimal: " << numberDecimal << std::endl;
-    std::cout << "Number Octal: " << numberOctal << std::endl;
-    std::cout << "Number Hex: " << numberHex << std::endl;
-    std::cout << "Number Binary: " << numberBin << std::endl;
-    
+    std::cout << "Number Decimal: " << number_decimal << std::endl;
+    std::cout << "Number Octal: " << number_octal << std::endl;
+    std::cout << "Number Hex: " << number_hex << std::endl;
+    std::cout << "Number Binary: " << number_bin << std::endl;
 
     return 0;
 }
