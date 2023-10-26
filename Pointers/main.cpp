9184f2562b10757e83c25db640dac26fe3324658
Leonardo Paz <estevam.leonardopaz@gmail.com>
2023-10-26 10:05:55 -0300

    Pointers
    Signed-off-by: Leonardo Paz <estevam.leonardopaz@gmail.com>

---
+++ Pointers/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int *p_number{}; // Same size
+    double *p_fractional_number{}; // Same size
+
+    int *p_null{nullptr}; // Initializing to null
+
+    std::cout << p_number << std::endl;
+    std::cout << p_null << std::endl;
+
+    int var{43};
+    int *pointer_var{&var};
+    std::cout << pointer_var << std::endl;
+    std::cout << *pointer_var << std::endl;
+
+    /*
+    int var{43};
+    double *pointer_var{&var}; compile error, pointer has to be in the same type.
+    */
+
+
+    return 0;
+}
