f81937039b7090611a8ddf85efd42f02e113b0ce
Leonardo Paz <estevam.leonardopaz@gmail.com>
2023-11-09 16:34:13 -0300

    lambda functions capture list
    Signed-off-by: Leonardo Paz <estevam.leonardopaz@gmail.com>

--- LambdaFunctions/main.cpp
+++ LambdaFunctions/main.cpp
@@ -9,5 +9,37 @@ int main(int argc, char const *argv[])
     [](double a, double b) -> void
     { std::cout << "a + b = " << (a + b) << std::endl; }(12.5, 5.7);
 
+    auto func1 = [](double a, double b) -> double // Cannot type the func1
+    { return a + b; };
+    std::cout << func1(10.5, 20.5) << std::endl;
+
+    double result = func1(11.5, 12.5);
+    std::cout << result << std::endl;
+
+    // Capture list by value
+    int c{42};
+    auto func_value = [c]() -> void
+    { std::cout << c << std::endl; };
+    // Capture list all by value
+    auto func_value_all = [=]() -> void
+    { std::cout << c << std::endl; };
+    // Capture list by refence
+    auto func_reference = [&c]() -> void
+    { std::cout << c << std::endl; };
+    // Capture list all by refence
+    auto func_reference_all = [&]() -> void
+    { std::cout << c << std::endl; };
+
+    std::cout << "--------------------" << std::endl;
+    for (size_t i = 0; i < 6; ++i)
+    {
+        func_value();
+        func_value_all();
+        func_reference();
+        func_reference_all();
+        ++c;
+        std::cout << "--------------------" << std::endl;
+    }
+
     return 0;
 }
