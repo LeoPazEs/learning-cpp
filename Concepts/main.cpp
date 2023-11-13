7607d8d62a7b483a90db90eb14dfbb421306ef91
Leonardo Paz <estevam.leonardopaz@gmail.com>
2023-11-13 14:58:00 -0300

    creating your own concepts
    Signed-off-by: Leonardo Paz <estevam.leonardopaz@gmail.com>

--- Concepts/main.cpp
+++ Concepts/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <concepts>
 
+/*
 template <typename T>
 requires std::integral<T>
 T add(T a, T b){
     return a + b;
+}
+*/
+
+// Create your own concept
+template <typename T>
+concept Multipliable = requires(T a, T b) {
+    a *b; // Just check valid syntax
+};
+
+template <typename T>
+T add(T a, T b)
+    requires std::integral<T>
+{
+    return a + b;
+}
+
+template <typename T>
+T mult(T a, T b)
+    requires Multipliable<T>
+{
+    return a * b;
 }
 
 int main(int argc, char const *argv[])
@@ -15,7 +37,7 @@ int main(int argc, char const *argv[])
     std::cout << "result_a : " << static_cast<int>(result_a) << std::endl;
 
     /*
-    Error pq nao ẽ um integral-> 
+    Error pq nao ẽ um integral->
     double b_0{10.1};
     double b_1{20.2};
     auto result_b = add(b_0, b_1);
@@ -25,11 +47,19 @@ int main(int argc, char const *argv[])
     int c_0{10};
     int c_1{20};
     auto result_c = add(c_0, c_1);
-    std::cout << "result_c : " << static_cast<int>(result_c) << std::endl;
+    std::cout << "result_c : " << result_c << std::endl;
 
+    int d_0{10};
+    int d_1{20};
+    auto result_d = mult(d_0, d_1);
+    std::cout << "result_d : " << result_d << std::endl;
 
+    /*
+    std::string e_0{10};
+    std::string e_1{20};
+    auto result_e = mult(e_0, e_1);
+    std::cout << "result_e : " << result_e << std::endl;
+    */
 
-
-    
     return 0;
 }
