cb3e878f63b4008779f412d9bab9399a8670d7ef
Leonardo Paz <estevam.leonardopaz@gmail.com>
2023-11-13 15:30:41 -0300

    logical operatos concepts
    Signed-off-by: Leonardo Paz <estevam.leonardopaz@gmail.com>

--- Concepts/main.cpp
+++ Concepts/main.cpp
@@ -29,20 +29,29 @@ T mult(T a, T b)
     return a * b;
 }
 
-//Compound requirement
+// Compound requirement
 template <typename T>
-concept Addable = requires (T a, T b) {
-	//noexcept is optional
-	{a + b} -> std::convertible_to<int>; //Compound requirement
-	//Checks if a + b is valid syntax, doesn't throw expetions(optional) , and the result
-	//is convertible to int(optional)
+concept Addable = requires(T a, T b) {
+    // noexcept is optional
+    {
+        a + b
+    } -> std::convertible_to<int>; // Compound requirement
+    // Checks if a + b is valid syntax, doesn't throw expetions(optional) , and the result
+    // is convertible to int(optional)
 };
 
 template <typename T>
-concept TinyType = requires (T t){
-    sizeof(T) <= 4; // Simple requirement : Only enforces syntax
+concept TinyType = requires(T t) {
+    sizeof(T) <= 4;          // Simple requirement : Only enforces syntax
     requires sizeof(T) <= 4; // Nested requirements
 };
+
+template <typename T>
+T add_requires(T a, T b)
+    requires std::integral<T> && TinyType<T> // Requires with logical operatos
+{
+    return a + b;
+}
 
 int main(int argc, char const *argv[])
 {
